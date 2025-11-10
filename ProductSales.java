bf0d8a4be8d6525134a5225e7b81a9ce8d947a48
NtandoJunior <168484144+NtandoJunior@users.noreply.github.com>
2025-11-10 13:41:05 +0200

    Update ProductSales.java

--- ProductSales.java
+++ ProductSales.java
@@ -1,1 +1,53 @@
+public class ProductSales implements IProduct {
+
+    @Override
+    public int TotalSales(int[][] productSales) {
+        int total = 0;
+        for (int[] year : productSales) {
+            for (int sale : year) {
+                total += sale;
+            }
+        }
+        return total;
+    }
+
+    @Override
+    public double AverageSales(int[][] productSales) {
+        int total = 0;
+        int count = 0;
+        for (int[] year : productSales) {
+            for (int sale : year) {
+                total += sale;
+                count++;
+            }
+        }
+        return (double) total / count;
+    }
+
+    @Override
+    public int MaxSale(int[][] productSales) {
+        int max = productSales[0][0];
+        for (int[] year : productSales) {
+            for (int sale : year) {
+                if (sale > max) {
+                    max = sale;
+                }
+            }
+        }
+        return max;
+    }
+
+    @Override
+    public int MinSale(int[][] productSales) {
+        int min = productSales[0][0];
+        for (int[] year : productSales) {
+            for (int sale : year) {
+                if (sale < min) {
+                    min = sale;
+                }
+            }
+        }
+        return min;
+    }
+}
 
