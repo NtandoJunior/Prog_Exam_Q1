9b64ba9abf89cb354f37d188c3c61a0540d11540
NtandoJunior <168484144+NtandoJunior@users.noreply.github.com>
2025-11-10 13:42:04 +0200

    Update ProductSalesReport.java

--- ProductSalesReport.java
+++ ProductSalesReport.java
@@ -1,1 +1,27 @@
+public class ProductSalesReport {
+    public static void main(String[] args) {
+        
+        int[][] sales = {
+            {300, 150, 700},  
+            {250, 200, 600}  
+        };
+
+        ProductSales ps = new ProductSales();
+        
+        int total = ps.TotalSales(sales);
+        double average = ps.AverageSales(sales);
+        int max = ps.MaxSale(sales);
+        int min = ps.MinSale(sales);
+
+        
+        System.out.println("=================================");
+        System.out.println("PRODUCT SALES REPORT - 2025");
+        System.out.println("=================================");
+        System.out.println("Total sales: " + total);
+        System.out.printf("Average sales: %.0f\n", average);
+        System.out.println("Maximum sale: " + max);
+        System.out.println("Minimum sale: " + min);
+        System.out.println("=================================");
+    }
+}
 
